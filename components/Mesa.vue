8a1d028cce1aebeeac737ec1dfa7d06b57b3b22e
DonizeteVida <donizetevida1998@outlook.com>
2025-06-08 21:40:53 +0200

    Add Mesa component

---
+++ components/Mesa.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        name: string
+        status: number
+    }>()
+
+    const name = props.name
+    const status = props.status
+</script>
+
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <h5>{{ status }}</h5>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        border: 1px solid black;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+    }
+</style>
--- pages/mesas/index.vue
+++ pages/mesas/index.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+    const mesas = [
+        {
+            name: "Mesa 1", status: 3
+        },
+        {
+            name: "Mesa 2", status: 2
+        },
+        {
+            name: "Mesa 3", status: 1
+        },
+        {
+            name: "Mesa 4", status: 0
+        }
+    ]
+</script>
+
 <template>
-    <h1>
-        MESAS AQUI
-    </h1>
+    <Mesa
+        v-for="mesa in mesas"
+        :name="mesa.name"
+        :status="mesa.status" />
 </template>
