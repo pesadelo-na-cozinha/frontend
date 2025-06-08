8b651752f5501ac323b1a880eeed1e773f390b31
DonizeteVida <donizetevida1998@outlook.com>
2025-06-08 21:18:23 +0200

    Add pages folder

---
+++ pages/mesas/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    const route = useRoute()
+    const id = route.params.id
+</script>
+
+<template>
+    <h1>
+        VAMOS VER A MESA {{ id }}
+    </h1>
+</template>
---
+++ pages/mesas/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        MESAS AQUI
+    </h1>
+</template>
